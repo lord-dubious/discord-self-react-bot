0fd49f26ba571d910bcd2ae46ef49113f0619fa3
chukwudubem chukwurah <chukwurahdavid@gmail.com>
2024-04-03 05:14:46 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="discord-self-react-bot",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="chukwudubem chukwurah",
+    author_email="chukwurahdavid@gmail.com",
+    description="discord reaction bot",
+    url="https://github.com/dubious-senpai/discord-self-react-bot",
+)
