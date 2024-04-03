82b4ba7af51abf8fcec8aa51c31df73b873437c4
chukwudubem chukwurah <chukwurahdavid@gmail.com>
2024-04-03 07:38:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
+    with open("README.md", "r") as fh:
+    long_description = fh.read()
     name="discord-self-react-bot",
     version="0.1",
     packages=find_packages(),
@@ -9,4 +11,11 @@ setup(
     author_email="chukwurahdavid@gmail.com",
     description="discord reaction bot",
     url="https://github.com/dubious-senpai/discord-self-react-bot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
 )
